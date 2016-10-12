f5ee73431484b72916a50344904b47b216ecb5d4
thiranon <thiranon.wi@gmail.com>
2016-10-12 21:38:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,3 +1,53 @@
 <?php
-  echo "Hi, I am BOT";
+$access_token = 'ss7WLvH2/cBC59tpgUiJ3ub1RHp6H35oJl0MVZBYgUcD679pgP/76jca7QXlveqmicW1V8JGQSv0rMn0BOr9fPdq4WjV3mD+++itZNNxqeKFfanEtuY/3BIPen4tSWPKsV1kO2Y2KzZtqDnyuAPRiAdB04t89/1O/w1cDnyilFU=';
+$proxy = '	http://fixie:k5WP2SqAZOdD7Ee@velodrome.usefixie.com:80';
+$proxyauth = 'fixie:k5WP2SqAZOdD7Ee';
+
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";
+
 ?>
